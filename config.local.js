269aed6ff7c3f7a39aa04c9bd5588b2d71836049
Eldor <islomjonoveldor79@gmail.com>
2026-04-13 21:45:37 +0500

    Add files via upload

---
+++ config.local.js
@@ -0,0 +1,7 @@
+window.TECHSHOP_CONFIG = {
+  SUPABASE_URL: "https://bigpkjupiimfjfethfxz.supabase.co",
+  SUPABASE_KEY: "sb_publishable_Bof7iXPjl_WSq1fceKi_6w_iQhf09cq",
+  IK_URL: "https://ik.imagekit.io/zaveeqnja",
+  IK_PUBLIC_KEY: "public_P73tVHOV8MbhGgBny85iUrMtcL4=",
+  TG_USERNAME: "yourusername"
+};
